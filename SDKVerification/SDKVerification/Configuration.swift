ee95a1c50d50542d77c7d1b70dbc90913284be7e
JONGILKIM <jongilkim@JONGILKIM-MacBook-Pro.local>
2018-03-23 11:18:34 +0900

    Host  하드코딩 내용 제

--- SDKVerification/SDKVerification/Configuration.swift
+++ SDKVerification/SDKVerification/Configuration.swift
@@ -10,9 +10,11 @@ import Foundation
 import simple
 
 
+//sample
+//let HOST = Host(serviceName: "devicesdk", deviceName: "IOS", deviceToken: "ae63a9402b5d11e8b50f", host:"api.sktiot.com", port: "8883", clientId: "ae63a9402b5d11e8b50f", userName: "ae63a9402b5d11e8b50f", password: nil, usingSSL: true, cleanSession: true)
+//sample
+//let CSV_HOST = Host(serviceName: "smartweather", deviceName: "ioscsv", deviceToken: "851ad800db1311e7b5bb", host:"api.sktiot.com", port: "8883", clientId: "851ad800db1311e7b5bb", userName: "851ad800db1311e7b5bb", password: nil, usingSSL: true, cleanSession: true)
 
-let HOST = Host(serviceName: "devicesdk", deviceName: "IOS", deviceToken: "ae63a9402b5d11e8b50f", host:"api.sktiot.com", port: "8883", clientId: "ae63a9402b5d11e8b50f", userName: "ae63a9402b5d11e8b50f", password: nil, usingSSL: true, cleanSession: true)
+let HOST = Host(serviceName: "", deviceName: "", deviceToken: "", host:"", port: "8883", clientId: "", userName: nil, password: nil, usingSSL: true, cleanSession: true)
 
-//let HOST = Host(serviceName: "", deviceName: "", deviceToken: "", host:"iot.eclipse.org", port: "8883", clientId: "go-simple", userName: nil, password: nil, usingSSL: true, cleanSession: true)
-
-let CSV_HOST = Host(serviceName: "smartweather", deviceName: "ioscsv", deviceToken: "851ad800db1311e7b5bb", host:"api.sktiot.com", port: "8883", clientId: "851ad800db1311e7b5bb", userName: "851ad800db1311e7b5bb", password: nil, usingSSL: true, cleanSession: true)
+let CSV_HOST = Host(serviceName: "", deviceName: "", deviceToken: "", host:"", port: "8883", clientId: "", userName: "", password: nil, usingSSL: true, cleanSession: true)
